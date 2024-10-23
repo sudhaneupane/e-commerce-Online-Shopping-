7b4c80df4a73b5833b3590bd807cbc1223e46275
sudhaneupane <sudaneupane.11@gmail.com>
2024-10-23 16:14:10 +0545

    navbar

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,15 @@
+import forever from "./forever.png";
+import search_icon from "./search-icon.webp";
+import profile_icon from "./profile-icon.webp";
+import cart_icon from "./cart-icon.webp";
+import menu_icon from "./menu-icon.webp";
+import back_icon from "./back-icon.webp"
+
+export const assets = {
+  forever,
+  search_icon,
+  profile_icon,
+  cart_icon,
+  menu_icon,
+  back_icon
+};
