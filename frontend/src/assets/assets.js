39838e764014c14d17600851e5570e0419784574
sudhaneupane <sudaneupane.11@gmail.com>
2024-10-23 20:16:59 +0545

    hero page

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -3,7 +3,8 @@ import search_icon from "./search-icon.webp";
 import profile_icon from "./profile-icon.webp";
 import cart_icon from "./cart-icon.webp";
 import menu_icon from "./menu-icon.webp";
-import back_icon from "./back-icon.webp"
+import back_icon from "./back-icon.webp";
+import hero_pic from "./hero-pic.jpg";
 
 export const assets = {
   forever,
@@ -11,5 +12,6 @@ export const assets = {
   profile_icon,
   cart_icon,
   menu_icon,
-  back_icon
+  back_icon,
+  hero_pic,
 };
