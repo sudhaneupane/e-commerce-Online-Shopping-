105befc890cee819f82891ebb8c3031f9572724a
sudhaneupane <sudaneupane.11@gmail.com>
2024-10-24 15:42:40 +0545

    latest collections

---
+++ frontend/src/components/LatestCollections.jsx
@@ -0,0 +1,39 @@
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+const LatestCollections = () => {
+  const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
+
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, []);
+
+  return (
+    <div className="my-3">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Explore Fresh Styles in Our Latest Collection!
+        </p>
+      </div>
+
+      {/* rendering products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((items, index) => (
+          <ProductItem
+            key={index}
+            id={items._id}
+            name={items.name}
+            image={items.image}
+            price={items.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LatestCollections;
