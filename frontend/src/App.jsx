772e2181163ead5dd457aee5b05649c39de19447
sudhaneupane <sudaneupane.11@gmail.com>
2024-10-24 15:52:31 +0545

    few fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,6 +19,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
         <Route path="/product/:productId" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
