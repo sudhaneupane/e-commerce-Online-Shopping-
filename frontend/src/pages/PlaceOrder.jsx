71dc2fa9d0c9cc5cef0899089b44ac2c9a4252ba
sudhaneupane <sudaneupane.11@gmail.com>
2024-11-15 10:40:30 +0545

    place Order

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Title from "../components/Title";
 import CartTotal from "../components/CartTotal";
 import { assets } from "../assets/assets";
+import { ShopContext } from "../context/ShopContext";
 
 const PlaceOrder = () => {
+  const [method, setMethod] = useState("cod");
+  const { navigate } = useContext(ShopContext);
   return (
     <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
       {/* left side  */}
@@ -73,13 +76,30 @@ const PlaceOrder = () => {
         <div className="mt-12">
           <Title text1={"Payment"} text2={"Methods"} />
           {/* payment method selection */}
-          {/* <div className="flex gap-3 flex-col lg:flex-row">
+          <div
+            className="flex gap-3 flex-col lg:flex-row"
+            onClick={() => setMethod("cod")}
+          >
             <div className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-              <p className={`min-w-3.5 h-3.5 border rounded-full`}>
-                <img className="h-5 mx-4" src={assets.return_policy_icon} alt="" />
+              <p
+                className={`min-w-3.5 h-3.5 border rounded-full ${
+                  method === "cod" ? "bg-green-400" : ""
+                }`}
+              ></p>
+              <p className="text-gray-700 text-sm font-medium mx-4">
+                Cash on delivery
               </p>
             </div>
-          </div> */}
+          </div>
+          <div className="w-full text-end mt-8">
+            {" "}
+            <button
+              onClick={() => navigate("/orders")}
+              className="bg-black text-white px-16 py-3 text-sm"
+            >
+              Place Order
+            </button>
+          </div>
         </div>
       </div>
     </div>
