a0ff006e52cf8af117ff32e0b2c79f91057e4e91
sudhaneupane <sudaneupane.11@gmail.com>
2024-11-15 13:34:40 +0545

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// endpoint
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(port, () => console.log(`Server started on ${port}`));
