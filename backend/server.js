df968ad5ffea3393f798a1b17daa83782fd2e30b
sudhaneupane <sudaneupane.11@gmail.com>
2024-11-16 10:24:39 +0545

    cloudinary configured

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectdb from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
-connectdb()
+connectdb();
+connectCloudinary();
 // middlewares
 app.use(express.json());
 app.use(cors());
