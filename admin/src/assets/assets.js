77bf210c44725d366e4e66d26652f7cd9cd0966b
sudhaneupane <sudaneupane.11@gmail.com>
2024-11-20 09:11:19 +0545

    admin add item

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -2,10 +2,12 @@ import forever from "./forever.png";
 import add_icon from "./add.png";
 import list_icon from "./down.png";
 import order_icon from "./file.png";
+import upload_icon from "./upload.png";
 
 export const assets = {
   forever,
   add_icon,
-  list_icon, order_icon
-
+  list_icon,
+  order_icon,
+  upload_icon
 };
