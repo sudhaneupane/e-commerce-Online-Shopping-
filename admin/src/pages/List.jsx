9969b634d9b766adcb2b5b1b52ce1dda043fbe5b
sudhaneupane <sudaneupane.11@gmail.com>
2024-11-21 13:26:30 +0545

    list product

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,7 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { backendUrl, currency } from "../App";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+let token = localStorage.getItem("token");
 
 const List = () => {
-  return <div></div>;
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(`${backendUrl}/api/products/list`, {
+        headers: { Authorization: `${token}` },
+      });
+
+      setList(response.data.products);
+      if (response.status == 200) {
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  const removeProduct = async (id) => {
+    try {
+      const response = await axios.delete(
+        `${backendUrl}/api/products/remove/${id}`,
+        { headers: { Authorization: `${token}` } }
+      );
+
+      if (response.status == 200) {
+        toast.success(response.data.message);
+        await fetchList();
+      } else {
+        toast.error(response.data.message);
+      }
+
+      console.log(response);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  return (
+    <>
+      <p className="mb-2">All Products</p>
+      <div className="flex flex-col gap-2">
+        {/* list table title */}
+        <div className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b className="text-center">Action</b>
+        </div>
+
+        {/* product list  */}
+        {list.map((item, index) => (
+          <div
+            className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm"
+            key={index}
+          >
+            <img className="w-12" src={item.image[0]} alt="" />
+            <p>{item.name} </p>
+            <p>{item.category} </p>
+            <p>
+              {currency}
+              {item.price}
+            </p>
+            <p
+              onClick={() => removeProduct(item._id)}
+              className="text-right md:text-center cursor-pointer text-lg"
+            >
+              X
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default List;
